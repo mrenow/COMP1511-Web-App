cdfda54bfb1b8048cc960ded621e9fad53348e6e
mr_enow <other.ezhui@gmail.com>
2019-10-04 17:17:00 +1000

    added placeholders

---
+++ server/messaging_test.py
@@ -0,0 +1,7 @@
+import pytest
+from server.server import *
+def invalid_token_test():
+    user = auth_login(TEST_EMAIL, TEST_PASSWORD)
+    auth_logout(user)
+    with pytest.raises(ValueError):
+        message(user, TEST_VALID_CHANNEL)
--- tests/messaging_test.py
+++
@@ -1,4 +0,0 @@
-import pytest
-import server.server
-def invalid_token_test():
-    auth_login
