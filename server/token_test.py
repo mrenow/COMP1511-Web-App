05df9ab104d86a2622ae44cf27ec69d07caa5f71
mr_enow <other.ezhui@gmail.com>
2019-10-06 19:42:29 +1100

    Iteration 1 final

--- server/token_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from server.server import *
-
-def invalid_token_test():
-    user = auth_login(TEST_EMAIL, TEST_PASSWORD)
-    auth_logout(user)
-    with pytest.raises(ValueError):
-        message(user, TEST_VALID_CHANNEL_ID)
