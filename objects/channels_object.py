5c0de66241a8124b8c4a8d5472ff072cf862129f
z5236552 <albertyeh199909@gmail.com>
2019-10-24 04:05:39 +1100

    completed class fucntions

--- objects/channels_object.py
+++ objects/channels_object.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 from messages import message
+from server.server import Users, Channels
 channel_id = 0
 
 class Channel:
     def __init__(self, name, owner, is_private):
         global channel_id
-        global users
+        global Users
+        global Channels
         self.name = name
-        self.owners = (dict(u_id = owners,
-        first_name = users[owner].get_name_first,
-        last_name = users[owners].get_name_last))
-        self.members = ()
+        self.owners = set(owner)
+        self.members = set()
         self.is_private = is_private
         self.id = channel_id
         channel_id += 1 
-        self.message_list = []
+        self.message_list = ()
+        Channels[self.id] = self
+        Users[owner].get_channels.add(self.id)
+
 
     #messages(text,channel,time)
     def set_name(self, name):
@@ -32,5 +35,71 @@ class Channel:
     def get_is_private(self):
         return self.is_private
     
-    def send_message(self):
-        curr_message 
+    def send_message(self, text, sender):
+        curr_message = message(text, sender)
+        self.message_list.append(curr_message)
+        return curr_message.get_id
+        
+
+    def delete_message(self, message_id):
+        for x in self.message_list:
+            if x.get_id == message_id:
+                self.message_list.remove(x)
+                break
+    
+    def join(self, u_id):
+        self.members.add(u_id) 
+        global Users
+        Users[u_id].get_channels.add(self.channel_id) 
+
+    def details(self):
+        global Users
+        for x in self.owners:
+             = dict(u_id = x,
+            first_name = Users[x].get_name_first,
+            last_name = Users[x].get_name_last)
+        owner_members = ()
+        owner_members.append()
+
+        for x in self.members:
+             = dict(u_id = x,
+            first_name = Users[x].get_name_first,
+            last_name = Users[x].get_name_last)   
+        members = ()
+        members.append()
+        details = dict(name = self.name,
+        owner_members = owner_members,
+        members = members
+        )
+
+    def leave(self, u_id):
+        global Users
+        for x in self.members:
+            if x == u_id:
+                self.members.discard(x) 
+        for x in self.owners:
+            if x == u_id:
+                self.owners.discard(x)
+        Users[u_id].get_channels.discard(self.channel_id)
+
+
+    def add_owner(self, u_id):
+        self.owners.add(u_id)            
+        global Users
+        Users[u_id].get_owners.add(self.channel_id)
+
+    def remove_owner(self, u_id):
+        for x in self.owners:
+            if x == u_id:
+                self.owners.discard(x)
+
+   
+
+        
+'''
+owner = dict(u_id = owners,
+        first_name = users[owners].get_name_first,
+        last_name = users[owners].get_name_last)owner = dict(u_id = owners,
+        first_name = users[owners].get_name_first,
+        last_name = users[owners].get_name_last)
+'''
