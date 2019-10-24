65ce8a8967fa7b8f003e98235e622814243746f5
Jason Xing <jasonxing2140@gmail.com>
2019-10-25 02:35:14 +1100

    draft done

--- objects/users_object.py
+++ objects/users_object.py
@@ -11,23 +11,45 @@ class User:
         self._email = email
         self._handle_str = name_first + name_last
         self._profile_picture = None
+        self._channels = {}
         user_count += 1
 
     def get_name_first(self, user):
         return self._name_first
 
+    def set_name_first(self, user, name_first):
+        self._name_first = name_first
+
     def get_name_last(self, user):
         return self._name_last
 
+    def set_name_last(self, user, name_last):
+        self._name_last = name_last
+    
     def get_email(self, user):
         return self._email
 
-    def set_user_profile(self, name_first, name_last):
-        pass
-    
-    
+    def set_email(self, user, email):
+        self._email = email
+
+    def get_channels(self, user):
+        return self._channels
+
+    def add_channel(self, user, channel_id):
+        global channels
+        self._channels[channel_id] = channels[channel_id]
+
+    def get_handle_str(self, user):
+        return self._handle_str
+
+    def set_handle_str(self, user, handle_str):
+        self._handle_str = handle_str
+
     def get_user_profile(self, user):
-        return self._email, self._name_first, self._name_last, self._handle_str
+        return dict(email = self._email, 
+                    name_first = self._name_first, 
+                    name_last = self._name_last,
+                    handle_str = self._handle_str)
     
     def to_json(self, user):
         return dict(u_id = self._u_id, 
