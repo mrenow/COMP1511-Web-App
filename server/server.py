e8c90f0d21221c6fb60ef5055e1fef307bf7e606
z5236552 <albertyeh199909@gmail.com>
2019-10-27 04:35:24 +1100

    plan B

--- server/server.py
+++ server/server.py
@@ -470,3 +470,292 @@ def admin_userpermission_change(token, u_id, permission_id):
     return {}
 
 
+
+
+
+
+
+
+
+
+
+# { react_id, u_ids, is_this_user_reacted } datetime
+
+MAX_LEN = 1000
+class Message:
+
+    def __init__(self, text, channel, sender, time = datetime.now()):
+        if MAX_LEN < text:
+            raise ValueError(f"message.__init__: '{text[:10]}...' exceeds maximum allowable length.") 
+        self._message = text
+        self._u_id = sender
+        self._channel_id = channel
+        self._time_created = time
+        self._is_sent = time <= datetime.now()
+        self._message_id = num_messages
+        self._is_pinned = False
+        self._reacts = {} # Dictionary of react id: react object.
+
+        messages[self._u_id] = self 
+        inc_messages()
+        
+
+    
+    def get_time(self):
+        return self._time_created
+    
+    def get_id(self):
+        return self._message_id
+
+    def get_channel(self):
+        return self._channel_id
+
+    def get_user(self):
+        return self._u_id
+
+    def get_message(self):
+        return self._message
+    
+    def is_sent(self):
+        return self._is_sent
+
+    def is_pinned(self):
+        return self._is_pinned
+
+    def remove(self):
+        channels[self._channel_id].delete_message(self._message_id)
+        del messages[self._message_id]
+
+    def set_pin(self, pin):
+        self._is_pinned = pin
+
+    def add_react(self, user, react):
+        if react not in self._reacts:
+            self._reacts = react(react, user)
+        else:
+            self._reacts.get(react)._u_ids.append(user)
+    
+    def set_message(self, message):
+        self._message = message
+
+    def remove_react(self, user, react):
+        self._reacts.get(react)._u_ids.remove(user)
+        if not self._reacts.get(react).get_users():
+            del self._reacts[react]
+
+    # Returns the reacts list as in specification
+    def get_reacts(self, user): 
+        return [react.to_json(user) for react in self._reacts.values()]
+
+    def to_json(self, user):
+        return dict(message_id = self._message_id,
+                    u_id = self._u_id,
+                    message = self._message,
+                    time_created  = self._time_created,
+                    reacts = self.get_reacts(user),
+                    is_pinned = self._is_pinned)
+class react:
+    def __init__(self, id, user):
+        self._u_ids = set([user])
+        self._react_id = id
+
+    def to_json(self, user):
+        return dict(u_ids = list(self._u_ids),
+                    react_id = self._react_id,
+                    is_this_user_reacted = (user in self._u_ids))
+    def get_users(self):
+        return self._u_ids
+    
+    def get_id(self):
+        return self._react_id
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+class User:
+
+    def __init__(self, name_first, name_last, email, password):
+        self._u_id = user_count
+        self._password = password
+        self._name_first = name_first
+        self._name_last = name_last
+        self._email = email
+        if user_count == 0:
+            self._permission_id = OWNER
+        else:
+            self._permission_id = MEMBER
+        self._handle_str = name_first + name_last
+        self._profile_picture = None
+        self._channels = set()
+        self._admin_channels = set()
+        self._owner_channels = set()
+        inc_users()
+
+
+    def get_id(self):
+        return self._u_id
+        
+    def get_name_first(self):
+        return self._name_first
+
+    def set_name_first(self, name_first):
+        self._name_first = name_first
+
+    def get_name_last(self):
+        return self._name_last
+
+    def set_name_last(self, name_last):
+        self._name_last = name_last
+    
+    def get_email(self):
+        return self._email
+
+    def set_email(self, email):
+        self._email = email
+
+    def get_permission(self):
+        return self._permission_id
+
+    def set_permission(self, permission):
+        self._permission_id = permission
+
+    def get_channels(self):
+        return self._channels
+
+    def get_admin_channels(self):
+        return self._admin_channels
+    
+    def get_owner_channels(self):
+        return self._owner_channels
+    
+    def add_channel(self, channel_id):
+        self._channels.add(channel_id)
+
+    def add_admin_channel(self,  channel_id):
+        self._admin_channels.add(channel_id)
+
+    def add_owner_channel(self,  channel_id):
+        self._owner_channels.add(channel_id)
+    
+    def remove_channel(self, channel_id):
+        self._channels.discard(channel_id)
+
+    def remove_admin_channel(self,  channel_id):
+        self._admin_channels.discard(channel_id)
+
+    def remove_owner_channel(self,  channel_id):
+        self._owner_channels.discard(channel_id)
+
+    def get_handle_str(self):
+        return self._handle_str
+
+    def set_handle_str(self, handle_str):
+        self._handle_str = handle_str
+
+    def get_user_profile(self):
+        return dict(email = self._email, 
+                    name_first = self._name_first, 
+                    name_last = self._name_last,
+                    handle_str = self._handle_str)
+    
+    def to_json(self):
+        return dict(u_id = self._u_id, 
+                    name_first = self._name_first, 
+                    name_last = self._name_last)
+
+
+
+class Channel:
+    def __init__(self, name, owner, is_private):
+        self.name = name
+        self.owners = set([owner])
+        self.members = set()
+        self.is_private = is_private
+        self.id = num_channels
+        
+        self.message_list = []
+        
+        inc_channels()
+
+
+    #messages(text,channel,time)
+    def set_name(self, name):
+        self.name = name
+    
+    def set_is_private(self, is_private):
+        self.is_private = is_private
+    
+    def get_name(self):
+        return self.name
+    
+    def get_owner(self):
+        return self.owners
+
+    def get_is_private(self):
+        return self.is_private
+
+    def get_id(self):
+        return self.id
+    
+    def send_message(self, text, sender):
+        curr_message = Message(text, self.id, sender)
+        self.message_list.append(curr_message)
+        return curr_message.get_id()
+        
+
+    def delete_message(self, message_id):
+        # Raises value if messages_id is not in list.
+        self.message_list.remove(message_id)
+    
+    def join(self, u_id):
+        self.members.add(u_id) 
+        users[u_id].get_channels.add(self.id) 
+
+    def details(self):
+        owner_members = []
+        for x in self.owners:
+            d = dict(u_id = x,
+            first_name = users[x].get_name_first(),
+            last_name = users[x].get_name_last())
+            owner_members.append()
+        
+        members = []
+        for x in self.members:
+            d = dict(u_id = x,
+            first_name = users[x].get_name_first(),
+            last_name = users[x].get_name_last())   
+            members.append()
+        details = dict( name = self.name,
+                        owner_members = owner_members,
+                        members = members)
+        return details
+
+    def leave(self, u_id):
+        self.members.discard(u_id)
+        if u_id in self.owners:
+            self.owners.discard(u_id)
+        users[u_id].get_channels().discard(self.id)
+
+
+    def add_owner(self, u_id):
+        self.owners.add(u_id)          
+        users[u_id].get_owner_channels().add(self.id)
+
+    def remove_owner(self, u_id):
+        self.owners.discard(u_id)
+
+   
+
+
+
+
+
+
+
+
+
+
