2e707e6d1a0e959c8099ced043f62ec9f4633e84
mr_enow <other.ezhui@gmail.com>
2019-10-27 20:14:34 +1100

    final iter2

--- objects/channels_object.py
+++ objects/channels_object.py
@@ -59,13 +59,8 @@ class Channel:
         
     def join(self, u_id):
         self.members.add(u_id)
-<<<<<<< HEAD
         get_users()[u_id].get_channels().add(self.id) 
 
-=======
-        print(users)
-        users[u_id].get_channels.add(self.id) 
->>>>>>> master
 
     def details(self):
         owner_members = []
